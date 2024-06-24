bd3d519ecef19950c4926d6eb8a49fb673f9138c
Oscar Mora <oscgmora@gmail.com>
2024-06-23 22:17:09 -0400

    Add .env file

--- app.js
+++ app.js
@@ -34,9 +34,7 @@ app.use(
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const dev_db_url =
-	"mongodb+srv://rodayga:Gabito2001@cluster0.yaozal5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
